e3c2274c4398110ac67d564db1ce13648282c6d3
Lando <oserrano@mail.stmarytx.edu>
2015-02-26 20:37:09 -0600

    Assignment 4

--- src/FindPi.java
+++ src/FindPi.java
@@ -1,33 +1,41 @@
-import java.util.Scanner;
-import static java.util.concurrent.TimeUnit.NANOSECONDS;
-import java.math.BigDecimal;
-
+	import java.util.Scanner;
+	import static java.util.concurrent.TimeUnit.NANOSECONDS;
+	import java.math.BigDecimal;
 public class FindPi {
 
-	public static void main(String[] args) {
-		double n = 0, iterations, uIterations;
-		BigDecimal.valueOf(n);
-
+	
+		public static void main(String[] args){
+			double  n = 0, iterations,uIterations;
+			BigDecimal.valueOf(n);
+		
+			
+			
 		long startTime, endTime;
+			
+			Scanner input = new Scanner(System.in);
+		
+			System.out.println("Enter the amount of iterations to find PI");
+			uIterations = input.nextDouble();
+			
+			 startTime = System.nanoTime();
+					        for (iterations = 0 ; iterations <uIterations; iterations++)
+					        {
+					              n += Math.pow(-1,iterations )/((2 * iterations) + 1);
+					         }
+					      
+					       n = n*4;
+					        endTime = System.nanoTime();
 
-		Scanner input = new Scanner(System.in);
-
-		System.out.println("Enter the amount of iterations to find PI");
-		uIterations = input.nextDouble();
-
-		startTime = System.nanoTime();
-		for (iterations = 0; iterations < uIterations; iterations++) {
-			n += Math.pow(-1, iterations) / ((2 * iterations) + 1);
-		}
-
-		n = n * 4;
-		endTime = System.nanoTime();
-
-		long totalTime = endTime - startTime;
-
-		System.out.println(BigDecimal.valueOf(n) + " " + "\n It took "+ (double) NANOSECONDS.toMillis(totalTime) / 1000
-		+ " seconds to complete this operation");
-
+							long totalTime = endTime - startTime;
+					       
+					       System.out.println(BigDecimal.valueOf(n)+" "+"\nIt took "+ (double)NANOSECONDS.toMillis(totalTime)/1000+" seconds to complete this operation");
+			
+				
+				
+		}	
+			
 	}
+		
 
-}
+
+
